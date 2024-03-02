6147b189f8bd4e16e0275202e1453a6ab1252185
udK01 <dominik.muranyi@gmail.com>
2024-03-02 11:08:28 +0000

    Setup text-based database with Backend server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,89 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   const [backendData, setBackendData] = useState([{}]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [listChange, setListChange] = useState("");
 
   useEffect(() => {
-    fetch("/api")
+    fetch("/api/data")
       .then((response) => response.json())
       .then((data) => setBackendData(data));
-  }, []);
+    setListChange(false);
+  }, [listChange]);
+
+  const MAX = 10000;
+  const MIN = 1;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let userData = { username: username, password: password };
+    fetch("/api/login", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+
+    setListChange(true);
+  }
+
+  function randomise() {
+    setUsername(`randomUser${Math.floor(Math.random() * (MAX - MIN) + MIN)}`);
+    setPassword(Math.floor(Math.random() * (MAX - MIN) + MIN));
+  }
+
+  function deleteField(x) {
+    fetch("/api/delete", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(backendData[x]),
+    });
+    setListChange(true);
+  }
 
   return (
-    <div>
-      {typeof backendData.users === `undefined` ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => <p key={i}>{user}</p>)
-      )}
-    </div>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
+        </div>
+        <div className="buttons">
+          <button onClick={randomise}>Randomise</button>
+          <button className="submitBtn">Submit</button>
+        </div>
+      </form>
+      <div className="container">
+        {backendData === null ? (
+          <p>Loading...</p>
+        ) : (
+          backendData.map((user, i) => (
+            <div className="row" key={i}>
+              <div className="cell">{user.username}</div>
+              <div className="cell">{user.password}</div>
+              <button className="deleteBtn" onClick={() => deleteField(i)}>
+                Delete
+              </button>
+            </div>
+          ))
+        )}
+      </div>
+    </>
   );
 }
-
-export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,72 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
+app.use(bodyParser.json());
+const filePath = path.join(__dirname, "Database", "database.txt");
+
+app.get("/api/data", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(500).json({ error: "Failed to read data." });
+    } else {
+      const lines = data.trim().split("\n");
+      const jsonData = lines.map((line) => JSON.parse(line));
+      res.json(jsonData);
+    }
+  });
+});
+
+app.post("/api/delete", (req, res) => {
+  // Read File.
+  fs.readFile(filePath, "utf8", (err, data) => {
+    // Handle Errors.
+    if (err) {
+      console.error("Error reading file:", err);
+    } else {
+      // Parse data as JSON objects.
+      const lines = data.trim().split("\n");
+      const parsedData = lines.map((line) => JSON.parse(line));
+
+      // Filter out the line to be deleted.
+      const filteredData = parsedData.filter((obj) => {
+        return (
+          obj.username !== req.body.username ||
+          obj.password !== req.body.password
+        );
+      });
+
+      // Rewrite File With Filtered Data.
+      fs.writeFile(
+        filePath,
+        filteredData.map((obj) => JSON.stringify(obj)).join("\n"),
+        "utf8",
+        (err) => {
+          if (err) {
+            console.error("Error writing file:", err);
+            return;
+          }
+          console.log("Row deleted successfully.");
+        }
+      );
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const dataToAppend = JSON.stringify(req.body) + "\n";
+
+  fs.appendFile(filePath, dataToAppend, (err) => {
+    if (err) {
+      console.error("Error appending to file:", err);
+      res.status(500).json({ error: "Failed to append data." });
+    } else {
+      res.json({ message: "Data Appended!" });
+    }
+  });
 });
 
 app.listen(5000, () => {
